c312b4f5959662202f20d0484aa4adc8136d5001
Arturo Suárez <masuareb@hotmail.com>
2021-01-20 22:14:35 -0600

    Fetch car list from REST backend

--- src/components/Carlist.js
+++ src/components/Carlist.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 
 class Carlist extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { cars: [] };
+    }
+    componentDidMount() {
+        fetch('http://localhost:8080/api/cars')
+            .then((response) => response.json())
+            .then((responseData) => {
+                this.setState({
+                    cars: responseData._embedded.cars,
+                });
+            })
+            .catch(err => console.error(err));
+    }
     render() {
         return (
             <div></div>
