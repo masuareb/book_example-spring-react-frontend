563379060095dd9f4ad2d53b867e8242c5431825
Arturo Suárez <masuareb@hotmail.com>
2021-01-21 17:07:24 -0600

    Added snapshots tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import TestRenderer from 'react-test-renderer';
+import AddCar from './components/AddCar';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+it('renders a snapshot', () => {
+  const tree = TestRenderer.create(<AddCar/>).toJSON();
+  expect(tree).toMatchSnapshot();
 });
